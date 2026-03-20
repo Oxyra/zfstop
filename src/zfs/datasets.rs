83f8fc2c952e9c7d7316a9e5ac6836bc05862af0
Oxyra <oxy@xtranit.nl>
2026-03-20 21:42:34 +0100

    add sharenfs and sharesmb to datasets

--- src/zfs/datasets.rs
+++ src/zfs/datasets.rs
@@ -8,6 +8,14 @@ pub struct Dataset {
     pub compression: (String, String),
     pub recordsize: (String, String),
     pub compressratio: String,
+    pub sharenfs: String,
+    pub sharesmb: String,
+}
+
+impl Dataset {
+    pub fn is_shares(&self) -> bool {
+        self.sharenfs != "off" || self.sharesmb != "off"
+    }
 }
 
 pub fn list_datasets(pool: &str) -> Vec<Dataset> {
@@ -16,7 +24,7 @@ pub fn list_datasets(pool: &str) -> Vec<Dataset> {
             "list",
             "-H",
             "-o",
-            "name,used,avail,mountpoint,compression,source:compression,recordsize,source:recordsize,compressratio",
+            "name,used,avail,mountpoint,compression,source:compression,recordsize,source:recordsize,compressratio,sharenfs,sharesmb",
             "-r",
             pool
         ])
@@ -35,9 +43,17 @@ pub fn list_datasets(pool: &str) -> Vec<Dataset> {
                 used: parts.get(1).unwrap_or(&"").to_string(),
                 avail: parts.get(2).unwrap_or(&"").to_string(),
                 mountpoint: parts.get(3).unwrap_or(&"").to_string(),
-                compression: (parts.get(4).unwrap_or(&"").to_string(), parts.get(5).unwrap_or(&"").to_string()),
-                recordsize: (parts.get(6).unwrap_or(&"").to_string(), parts.get(7).unwrap_or(&"").to_string()),
+                compression: (
+                    parts.get(4).unwrap_or(&"").to_string(),
+                    parts.get(5).unwrap_or(&"").to_string()
+                ),
+                recordsize: (
+                    parts.get(6).unwrap_or(&"").to_string(),
+                    parts.get(7).unwrap_or(&"").to_string()
+                ),
                 compressratio: parts.get(8).unwrap_or(&"").to_string(),
+                sharenfs: parts.get(9).unwrap_or(&"off").to_string(),
+                sharesmb: parts.get(10).unwrap_or(&"off").to_string(),
             }
         })
     .collect()
